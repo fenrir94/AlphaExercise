5f24384ef12cacdf3f9aeb5987ef60a5e85c0426
Fenr1r <4p0the0sis94@gmail.com>
2025-05-23 15:47:08 +0900

    Modify main
    Copy barebones.cpp code

--- Alpha/main.cpp
+++ Alpha/main.cpp
@@ -1,5 +1,56 @@
 #include <iostream>
+#include "AEEngine.h"
+#include "pongGame.h"
 
-int main() {
-	return 0;
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+
+	int gGameRunning = 1;
+
+	// Initialization of your own variables go here
+
+	// Using custom window procedure
+	AESysInit(hInstance, nCmdShow, 1600, 900, 1, 60, false, NULL);
+
+	AEGfxSetBackgroundColor(0.0f, 0.0f, 0.0f);
+
+	// Changing the window title
+	AESysSetWindowTitle("PongGame");
+
+	// reset the system modules
+	AESysReset();
+
+	// Game Loop
+	while (gGameRunning)
+	{
+		// Informing the system about the loop's start
+		AESysFrameStart();
+
+		// Basic way to trigger exiting the application
+		// when ESCAPE is hit or when the window is closed
+		if (AEInputCheckTriggered(AEVK_ESCAPE) || 0 == AESysDoesWindowExist())
+			gGameRunning = 0;
+
+		// Your own update logic goes here
+
+
+		// Your own rendering logic goes here
+
+
+		// Informing the system about the loop's end
+		AESysFrameEnd();
+
+	}
+
+
+	// free the system
+	AESysExit();
 }
