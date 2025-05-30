1feff65f7a63e0411b86ba4020d8c50d3b5a3b32
Fenr1r <4p0the0sis94@gmail.com>
2025-05-30 18:49:21 +0900

    Modify pongGame
    - Modfiy Function call structure

--- Alpha/pongGame.cpp
+++ Alpha/pongGame.cpp
@@ -25,18 +25,15 @@ void pongGame::initialize()
 
 	this->widthTextTime = 0.0f;
 	this->heightTextTime = 0.0f;
+	
 }
 
 void pongGame::update()
 {
-
-	AESysFrameStart();
 	f64 dt = AEFrameRateControllerGetFrameTime();
 	this->playTime += dt;
-	//std::cout << playTime << std::endl;
-	AEGfxSetBackgroundColor(0.0f, 0.0f, 0.0f);
 
-
+	pongGame::convert_TimetoText(playTime);
 
 	/*if (AEInputCheckTriggered(AEVK_ESCAPE))
 	{
@@ -47,13 +44,7 @@ void pongGame::update()
 	if (pongGame::restart_GamePlay()) {
 		break;
 	}*/
-
-
-	pongGame::convert_TimetoText(playTime);
-	pongGame::print_Time();
-
-
-	AESysFrameEnd();
+	
 }
 
 void pongGame::print()
@@ -69,22 +60,19 @@ void pongGame::exit()
 
 void pongGame::convert_TimetoText(f64 playTime)
 {
-	std::cout << this->playTime << std::endl;
 	int currentTime = static_cast<int>(this->playTime);
-	std::cout << currentTime << std::endl;
 	int minutes = (currentTime % 3600) / 60;
 	int seconds = currentTime % 60;
-
-	std::cout << minutes << ":" << seconds << std::endl;
 
 	// Text to print
 	if (this->textPlayTime != nullptr) {
 		snprintf(textPlayTime, 10, "%02d:%02d", minutes, seconds);
 	}
+
+	AEGfxGetPrintSize(pongManager::pFont, textPlayTime, 1.f, &this->widthTextTime, &this->heightTextTime);
 }
 
 void pongGame::print_Time()
 {
-	AEGfxGetPrintSize(pongManager::pFont, textPlayTime, 1.f, &this->widthTextTime, &this->heightTextTime);
-	AEGfxPrint(pongManager::pFont, textPlayTime, -(this->widthTextTime) / 2, 1.f - (this->heightTextTime), 1, 1, 1, 1, 1);
+	AEGfxPrint(pongManager::pFont, textPlayTime, -(this->widthTextTime) / 2.f, 1.f - (this->heightTextTime), 1, 1, 1, 1, 1);
 }
