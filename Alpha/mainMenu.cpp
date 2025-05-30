94888ea618d8872ee3f9aaafea280869dafeff0c
Fenr1r <4p0the0sis94@gmail.com>
2025-05-30 11:37:34 +0900

    Add mainMenu
    - Add mainMenu.h and mainMenu.cpp
    - mainMenu is a concrete class of AGameState
    - Implement member functions

---
+++ Alpha/mainMenu.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "mainMenu.h"
+#include "pongManager.h"
+#include "utilityKey.h"
+
+mainMenu mainMenuState;
+
+mainMenu::mainMenu()
+{
+	this->textPress = "Press [Space] to Play";
+}
+
+mainMenu::~mainMenu()
+{
+}
+
+void mainMenu::initialize()
+{
+	//std::cout << "main Menu initialize" << std::endl;
+	AEGfxGetPrintSize(pongManager::pFont,this->textPress,1.f,&(this->widthText),&(this->heightText));
+}
+
+void mainMenu::update()
+{
+
+}
+
+void mainMenu::print()
+{
+	AEGfxSetBackgroundColor(0.f, 0.f, 0.f);
+	
+	AEGfxPrint(pongManager::pFont, this->textPress, -(this->widthText) / 2.f, -(this->heightText) / 2.f, 1, 1, 1, 1, 1);
+}
+
+void mainMenu::exit()
+{
+
+}
+
---
+++ Alpha/mainMenu.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "AEEngine.h"
+#include "AGameState.h"
+
+
+class mainMenu : public AGameState
+{
+public:
+	mainMenu();
+	~mainMenu();
+	
+	void initialize() override;
+	void update() override;
+	void print() override;
+	void exit() override;
+
+private:
+	f32 widthText, heightText;
+	const char* textPress;
+
+};
+
+extern mainMenu mainMenuState;
